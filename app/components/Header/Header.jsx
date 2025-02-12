79e2e4583aa51b21f46d0037a7ac018561c539b5
aisuluuduishebaeva <aisuluu_02@icloud.com>
2025-02-12 13:35:57 +0600

    fix

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,7 +1,49 @@
-export default function Header(){
-    return(
-        <header>
-            <h1>dawdaw</h1>
-        </header>
-    )
-}
+import { useState } from 'react';
+import Image from 'next/image';
+import './Header.css';
+import logo from './img/fone.svg';
+import quit from './img/Icon - Quit.svg';
+
+const Header = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
+
+  return (
+    <header className='header'>
+      <div className="container">
+        <div className="header__content">
+          <div className="header__deck">
+            <Image src={logo} alt="fone" />
+          </div>
+          <div className="header__title">
+            <Image
+              src={quit}
+              alt="quit"
+              className="header__quit"
+              onClick={togglePopup} 
+            />
+            <h3>Выход</h3>
+          </div>
+        </div>
+      </div>
+
+      {isPopupVisible && (
+        <div className="popup">
+          <div className="popup__content">
+            <h4>Вы уверены, что хотите выйти?</h4>
+            <button onClick={() => setIsPopupVisible(false)}>Отмена</button>
+            <button onClick={() => {
+              alert('Выход...');
+              setIsPopupVisible(false);
+            }}>Да</button>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
