acde1cb33c4ec3d2d9ff8bd09511c529bf6494c4
xrisent <ormonov31261@gmail.com>
2025-02-12 15:26:58 +0600

    awdaw

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -10,11 +10,11 @@ export default function AdminLogin() {
             <h1 className={montserrat.className} style={{ color: 'white' }}>Вход в аккаунт</h1>
             <div className="form">  {/* Убрали styles.form */}
                 <form>
-                    <div class="input-box">
+                    <div className="input-box">
                         <label>*Логин</label>
                         <input type="text" placeholder="Введите логин" />
                     </div>
-                    <div class="input-box">
+                    <div className="input-box">
                         <label>*Пароль</label>
                         <input type="password" placeholder="Введите пароль" />
 
