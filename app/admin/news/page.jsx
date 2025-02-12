a8519e44e621dc0aa907b74b4da33dd0240dea6c
xrisent <ormonov31261@gmail.com>
2025-02-12 13:32:46 +0600

    news

--- app/admin/news/page.jsx
+++ app/admin/news/page.jsx
@@ -1,3 +1,8 @@
+import './news.css'
+import './news.media.css'
+import addImg from './img/add.svg'
+import delImg from './img/del.svg'
+
 export default async function News() {
     const res = await fetch('https://ades.kg:8086/news/getAllNews', {
       next: { revalidate: 3600 }, // Обновлять кэш раз в час
@@ -5,17 +10,34 @@ export default async function News() {
     const news = await res.json();
   
     return (
-      <div>
-        <h1>Новости</h1>
-        <ul>
-          {news.map((item) => (
-            <li key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.content}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <section id="news">
+        <div className="container">
+          <div className="news__box">
+            <div className="news__box__header">
+              <h2>Новости</h2>
+              <div className="news__box__header-buttons">
+                <button>Добавить новость</button>
+                <button>Удалить</button>
+              </div>
+
+              <div className="news__box__header-buttons__mobile">
+                <button><img src={addImg.src} alt="" /></button>
+                <button><img src={delImg.src} alt="" /></button>
+              </div>
+            </div>
+            <div className="news__box__content">
+              {news?news.map((el, idx)=>(
+                <div className="news__box__content-card" key={idx}>
+                  <img src={`https://ades.kg:8086/${el.cover}`} alt="" />
+                  <p>{el.title}</p>
+                </div>
+              )):''}
+              
+            </div>
+          </div>
+        </div>
+        
+      </section>
     );
   }
   
